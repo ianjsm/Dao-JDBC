dd25418715372ef390a261225440b09e09f88d8d
Ian Soares <ian.soares@aluno.uece.br>
2024-03-09 15:17:34 -0300

    DAO Factory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,11 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 public class Main {
 	public static void main(String[] args) {
-
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao(); //injeção de dependência
 	}
 }
